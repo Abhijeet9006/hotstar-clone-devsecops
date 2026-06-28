e5e9bef8cbedfd9582e2394761fe34f3dbc9346a
Abhijeet <abhijeetkumar7765194@gmail.com>
2026-06-28 21:24:43 +0530

    Configure Terraform remote backend

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,6 +1,14 @@
 terraform {
   required_version = ">= 1.5.0"
 
+  backend "s3" {
+    bucket         = "abhi9006-hotstar-tfstate"
+    key            = "eks/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
